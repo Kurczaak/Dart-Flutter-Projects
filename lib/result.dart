050dc87c5b5c6145f60f06a37567e3d82e2eb064
kurczak <kurczakmichal97@gmail.com>
2020-08-17 18:53:43 +0100

    fixed last bugs

--- lib/result.dart
+++ lib/result.dart
@@ -1,31 +1,24 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  String score;
+  final int score;
   final Function resetHandler;
 
-  Result(int score, this.resetHandler) {
-    this.score = score.toString();
-  }
+  Result(this.score, this.resetHandler);
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(children: [
         Padding(padding: EdgeInsets.fromLTRB(0, 50, 0, 0)),
-        Text(
-            "Your score:",
+        Text("Your score:",
             style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
-        Text(
-            score,
+        Text(score.toString(),
             style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
         FlatButton(
-          child: Text(
-              "Restart Quiz!",
-          style: TextStyle(fontSize: 20)),
+          child: Text("Restart Quiz!", style: TextStyle(fontSize: 20)),
           onPressed: resetHandler,
           textColor: Colors.red,
-
         )
       ]),
     );
